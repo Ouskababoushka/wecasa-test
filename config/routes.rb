bbafda137db1d623aa01bfc42c29f00a9c015d21
Valentin Chauveau <valentin.korea@gmail.com>
2024-04-22 17:26:33 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'home/index'
+  root 'home#index'
+  resources :bookings, only: [:create]
+  get '/confirmation', to: 'bookings#confirmation'
+  post '/find_pro_for_existing_booking', to: 'bookings#find_pro_for_existing_booking', as: 'find_pro_bookings'
 end
