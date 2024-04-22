56fc6f7abc3c953d63cfb1f4b1e24c96eecc84d2
Valentin Chauveau <valentin.korea@gmail.com>
2024-04-22 13:02:46 +0200

    change association for professionnal model

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -1,6 +1,6 @@
 class Professional < ApplicationRecord
-  has_many :appointments
   has_many :services
   has_many :prestations, through: :services
+  has_many :appointments, dependent: :destroy
   has_many :opening_hours, dependent: :destroy
 end
