4dcbcca29343b3e300a5ad16a22e9713bf26528a
Valentin Chauveau <valentin.korea@gmail.com>
2024-04-23 12:38:21 +0200

    refacto home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
+  before_action :set_prestations, only: [:index]
+  before_action :set_existing_bookings, only: [:index]
+
   def index
+  end
+
+  def set_prestations
     @prestations = Prestation.all
+  end
+
+  def set_existing_bookings
     @existing_bookings = Booking.where(professional_id: nil).pluck(:id)
   end
 end
